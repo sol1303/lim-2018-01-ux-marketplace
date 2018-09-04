72c098b4bc971d47f98c2094cbced7f8593b5445
YennyCastillo <aricrisf@hotmail.com>
2018-09-04 07:31:18 -0500

    segunda vista

--- src/app.js
+++ src/app.js
@@ -3,3 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
  });
 
+ //carousel
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {
+    indicators: false,
+    interval: 2000,
+    height: 500,
+  });
+});
+
+
+
+
+
+
